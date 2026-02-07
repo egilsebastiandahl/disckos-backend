fcfaddd3b653cbf30918fe5d0d507ae87429eae3
egilsebastiandahl <egilsebastiandahl@outlook.com>
2026-02-07 12:55:05 +0100

    Initial files

---
+++ src/main/kotlin/com/example/disckosbackend/DisckosBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.disckosbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DisckosBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DisckosBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/disckosbackend/DisckosBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.disckosbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DisckosBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
