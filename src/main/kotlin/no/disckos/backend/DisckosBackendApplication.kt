cc2392433f1b236ede986fe0dd07a86cf93cb161
egilsebastiandahl <egilsebastiandahl@outlook.com>
2026-02-07 13:56:37 +0100

    Database is now configured, and everything runs

--- src/main/kotlin/no/disckos/backend/DisckosBackendApplication.kt
+++ src/main/kotlin/no/disckos/backend/DisckosBackendApplication.kt
@@ -1,4 +1,4 @@
-package com.example.disckosbackend
+package no.disckos.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/no/disckos/backend/DisckosBackendApplicationTests.kt
+++ src/test/kotlin/no/disckos/backend/DisckosBackendApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.disckosbackend
+package no.disckos.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
