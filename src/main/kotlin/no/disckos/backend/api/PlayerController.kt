8095b39e80ef0e1e1349355c2777f136dd977a61
egilsebastiandahl <egilsebastiandahl@outlook.com>
2026-04-29 01:23:52 +0200

    added check for verified players

--- src/main/kotlin/no/disckos/backend/api/PlayerController.kt
+++ src/main/kotlin/no/disckos/backend/api/PlayerController.kt
@@ -4,16 +4,19 @@ import no.disckos.backend.api.dto.player.PlayerResponse
 import no.disckos.backend.api.dto.player.toResponse
 import no.disckos.backend.application.player.GetPlayerHandler
 import no.disckos.backend.application.player.GetPlayersHandler
+import no.disckos.backend.repository.ProfileRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/player")
 class PlayerController(
     private val getPlayerHandler: GetPlayerHandler,
-    private val getPlayersHandler: GetPlayersHandler
+    private val getPlayersHandler: GetPlayersHandler,
+    private val profileRepository: ProfileRepository,
 ) {
     // Gets all the players
     @GetMapping
@@ -28,8 +31,8 @@ class PlayerController(
         else return player.toResponse()
     }
 
-
-
-
-
+    @GetMapping("/linked-ids")
+    fun getLinkedPlayerIds(): List<UUID> =
+        profileRepository.findAll()
+            .mapNotNull { it.player?.id }
 }
